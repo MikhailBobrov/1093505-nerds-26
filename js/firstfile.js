c02bb1208f68f2f6f1fc3d817f4f9ee2b0149bf8
mikhailbobrov <mikhailurban13@yandex.ru>
2019-10-22 21:21:38 +0500

    нашел ошибки, нужно исправлять

--- js/firstfile.js
+++ js/firstfile.js
@@ -1,2 +1,27 @@
-console.log(1)
+var link = document.querySelector(".map-button");
+var popup = document.querySelector(".modal-login");
+var close = document.querySelector(".popup-close");
+var form = popup.querySelector("form");
+var login = popup.querySelector("[name=login]");
+var email = popup.querySelector("[name=email]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  login.focus();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  console.log (login.value);
+  console.log (email.value);
+});
+
+
+
 
