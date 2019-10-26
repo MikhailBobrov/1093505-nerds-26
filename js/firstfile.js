4e2ffdbf92fac36ae1ca8d98e88599421aa5b55b
mikhailbobrov <mikhailurban13@yandex.ru>
2019-10-26 14:42:35 +0500

    js done

--- js/firstfile.js
+++ js/firstfile.js
@@ -5,21 +5,53 @@ var form = popup.querySelector("form");
 var login = popup.querySelector("[name=login]");
 var email = popup.querySelector("[name=email]");
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  login.focus();
+
+  if (storage) {
+    login.value = storage;
+    email.focus ();
+  } else {
+    login.focus();
+  }
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value){
     evt.preventDefault();
-    console.log ("Нужно ввести логин и email");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value)
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove ("modal-show");
+      popup.classList.remove("modal-error");
+    }
   }
 });
 
